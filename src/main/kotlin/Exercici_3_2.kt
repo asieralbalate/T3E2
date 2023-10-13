69682e048cffd76928e92ef675183fa0c607b5b0
asieralbalate <alualb1634@ieselcaminas.org>
2023-10-13 02:41:20 +0200

    final

--- src/main/kotlin/Exercici_3_2.kt
+++ src/main/kotlin/Exercici_3_2.kt
@@ -4,16 +4,16 @@ fun main(args: Array<String>) {
     val ruta1 = "Rutes.dat"
     val ruta2 = "Rutes.obj"
 
-    val rutas: MutableList<Ruta> = mutableListOf()
+    val rutas: MutableList<Ruta> = mutableListOf() //listado de todas las rutas
     try {
-        val input = DataInputStream(FileInputStream(ruta1))
-        val output = ObjectOutputStream(FileOutputStream(ruta2))
-        while (input.available() > 0) {
-            val nomRuta = input.readUTF()
+        val input = DataInputStream(FileInputStream(ruta1)) //para leer .dat
+        val output = ObjectOutputStream(FileOutputStream(ruta2)) //para escribir .obj
+        while (input.available() > 0) { //bucle fileinputstream
+            val nomRuta = input.readUTF() //lees cada tipo
             val denivell = input.readInt()
             val desnivellacumulat = input.readInt()
             val numPunts = input.readInt()
-            val listaDePunts: MutableList<PuntGeo> = mutableListOf()
+            val listaDePunts: MutableList<PuntGeo> = mutableListOf() //declaras una lista tiene que coicidir con PuntGeo
             for (i in 1..numPunts){
                 val nomPunt = input.readUTF()
                 val long = input.readDouble()
@@ -21,13 +21,13 @@ fun main(args: Array<String>) {
                 val punt = PuntGeo(nomPunt,Coordenades(long, lat))
                 listaDePunts.add(punt)
             }
-            val rutaArchiu = Ruta(nomRuta, denivell, desnivellacumulat, listaDePunts)
-            rutas.add(rutaArchiu)
+            val rutaArchiu = Ruta(nomRuta, denivell, desnivellacumulat, listaDePunts) //Escribe la ruta
+            rutas.add(rutaArchiu) //la añade a rutas
         }
-        for (ruta in rutas){
-            ruta.mostrarRuta()
+        for (ruta in rutas){ //for para mostrar las rutas
+            ruta.mostrarRuta()  //muestra
             println(" ")
-            output.writeObject(ruta)
+            output.writeObject(ruta) //escribe en el archivo todas las rutas
         }
         input.close()
         output.close()
